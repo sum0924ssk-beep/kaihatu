08fd2031dd0b4160f468638374e4198f86121291
しょうま <sum0924ssk@gmail.com>
2025-12-02 11:55:06 +0900

    no message

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import random
 APP_DATA_DIR = Path("./app_data")
 DB_NAME = APP_DATA_DIR / "condiments.db"
 UPLOAD_DIR = APP_DATA_DIR / "uploads"
-STATIC_DIR = Path("C:/Users/2250048/OneDrive - yamaguchigakuen/ドキュメント/kaihatu/static")
+STATIC_DIR = Path(__file__).parent / "static"
 # 期限切れが近いと見なす日数
 EXPIRY_THRESHOLD_DAYS = 7 
 
